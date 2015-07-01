12878a454152daf145a84b96540c2aa3655a8d35
Chris Forstall <cforstall@gmail.com>
2015-07-01 17:22:55 +0200

    tweak setup for ach

--- setup/vagrant.bootstrap.sh
+++ setup/vagrant.bootstrap.sh
@@ -2,39 +2,66 @@
 
 set -x
 
-. /vagrant/setup/tessrc
+TESSROOT="/home/vagrant/tesserae"
+TESSTMP="/home/vagrant/tess-session"
+TESSDOMAIN="stace.tesserae.org"
+TESSNCORES=2
 
 # get necessary packages
-
-apt-get update
 
 apt-get install -y \
 	git                 \
 	apache2             \
-	screen              \
-	vim                 \
 	htop                \
-	perl-doc            \
-	sqlite3             \
+    libapache2-mod-perl2 \
 	libapache2-mod-php5 \
 	libfile-copy-recursive-perl \
 	libparallel-forkmanager-perl \
 	liblingua-stem-perl \
-	libdbd-sqlite3-perl \
-   libarchive-zip-perl \
 	libjson-perl        \
 	libxml-libxml-perl  \
 	libgit-repository-perl \
-   python-numpy        \
-   python-scipy        \
-   python-pip
 
-pip install gensim
 
 # setup tesserae
 
-sudo -u vagrant git clone https://github.com/cwf2/tesserae $TESSROOT
+git clone -b unige.ach.2015 https://github.com/cwf2/tesserae $TESSROOT
 
-sudo -u vagrant /vagrant/setup/setup.tess.core.sh
+if ! [ -e $TESSTMP ]
+then
+   mkdir $TESSTMP
+fi
 
-sudo -u vagrant /vagrant/setup/setup.apache.sh
+perl $TESSROOT/scripts/configure.pl --url_root $TESSDOMAIN --fs_tmp $TESSTMP
+perl $TESSROOT/scripts/install.pl
+
+perl $TESSROOT/scripts/v3/build-stem-cache.pl
+perl $TESSROOT/scripts/v3/patch-stem-cache.pl
+
+# word index
+perl $TESSROOT/scripts/v3/add_column.pl --parallel $TESSNCORES $TESSROOT/texts/la/*
+
+# stem index
+perl $TESSROOT/scripts/v3/add_col_stem.pl --parallel $TESSNCORES $TESSROOT/texts/la/*
+
+# trigram index
+perl $TESSROOT/scripts/v3/add_col_stem.pl --parallel $TESSNCORES \
+   --feat 3gr $TESSROOT/texts/la/*
+
+perl $TESSROOT/scripts/v3/corpus-stats.pl --feat stem --feat 3gr la
+
+
+#
+# configure web server
+#
+
+perl $TESSROOT/scripts/apache2/vhost-gen.pl > /tmp/tesserae.conf
+mv /tmp/tesserae.conf /etc/apache2/sites-available/
+a2ensite tesserae
+service apache2 restart
+
+chgrp -R www-data $TESSTMP
+chmod g+w $TESSTMP
+
+# build dropdown lists
+perl $TESSROOT/scripts/v3/textlist.pl la
